dde0a8f5a2c1bf06fe376684351cf21b50038d11
yairsln <usis051921@ugb.edu.sv>
2022-11-06 15:34:53 -0600

    arreglos de carpeta

---
+++ ia.py
@@ -0,0 +1,76 @@
+from tkinter import *
+from tkinter import messagebox as msg
+import os
+import cv2
+from matplotlib import pyplot as plt
+from mtcnn.mtcnn import MTCNN
+import database as db
+
+
+path = "C:\Users\jairo\OneDrive\Desktop\Parcial-II" # your path
+txt_login = "Iniciar Sesión"
+txt_register = "Registrarse"
+
+color_white = "#f4f5f4"
+color_black = "#101010"
+
+color_black_btn = "#202020"
+color_background = "#151515"
+
+font_label = "Century Gothic"
+size_screen = "500x300"
+
+# colors
+color_success = "\033[1;32;40m"
+color_error = "\033[1;31;40m"
+color_normal = "\033[0;37;40m"
+
+res_bd = {"id": 0, "affected": 0} # database variable
+# GENERAL
+def getEnter(screen):
+    ''' Set an enter inside the screen '''
+    Label(screen, text="", bg=color_background).pack()
+
+def printAndShow(screen, text, flag):
+    ''' Prints and shows text '''
+    if flag:
+        print(color_success + text + color_normal)
+        screen.destroy()
+        msg.showinfo(message=text, title="¡Éxito!")
+    else:
+        print(color_error + text + color_normal)
+        Label(screen, text=text, fg="red", bg=color_background, font=(font_label, 12)).pack()
+
+def configure_screen(screen, text):
+    ''' Configure global styles '''
+    screen.title(text)
+    screen.geometry(size_screen)
+    screen.configure(bg=color_background)
+    Label(screen, text=f"¡{text}!", fg=color_white, bg=color_black, font=(font_label, 18), width="500", height="2").pack()
+
+def credentials(screen, var, flag):
+    ''' Configuration of user input '''
+    Label(screen, text="Usuario:", fg=color_white, bg=color_background, font=(font_label, 12)).pack()
+    entry = Entry(screen, textvariable=var, justify=CENTER, font=(font_label, 12))
+    entry.focus_force()
+    entry.pack(side=TOP, ipadx=30, ipady=6)
+
+    getEnter(screen)
+    if flag:
+        Button(screen, text="Capturar rostro", fg=color_white, bg=color_black_btn, activebackground=color_background, borderwidth=0, font=(font_label, 14), height="2", width="40", command=login_capture).pack()
+    else:
+        Button(screen, text="Capturar rostro", fg=color_white, bg=color_black_btn, activebackground=color_background, borderwidth=0, font=(font_label, 14), height="2", width="40", command=register_capture).pack()
+    return entry
+
+def face(img, faces):
+    data = plt.imread(img)
+    for i in range(len(faces)):
+        x1, y1, ancho, alto = faces[i]["box"]
+        x2, y2 = x1 + ancho, y1 + alto
+        plt.subplot(1,len(faces), i + 1)
+        plt.axis("off")
+        face = cv2.resize(data[y1:y2, x1:x2],(150,200), interpolation=cv2.INTER_CUBIC)
+        cv2.imwrite(img, face)
+        plt.imshow(data[y1:y2, x1:x2])
+
+        
