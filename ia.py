b8977bdd45b649938e1df153c5208a0655b75b89
WalterMartinez20 <smis133220@ugb.edu.sv>
2022-11-12 12:11:55 -0600

    Agregamos codigo de la IA

--- ia.py
+++ ia.py
@@ -1,3 +1,10 @@
+'''
+Descripcion: Login con reconocimiento facial
+Autores:
+Gerson Jair Solano Villegas
+Kevin Rodrigo Corcio Bonilla
+Walter Alberto Martinez Medrano
+'''
 from tkinter import *
 from tkinter import messagebox as msg
 import os
@@ -73,4 +80,135 @@ def face(img, faces):
         cv2.imwrite(img, face)
         plt.imshow(data[y1:y2, x1:x2])
 
-        
+# REGISTER #
+def register_face_db(img):
+    name_user = img.replace(".jpg","").replace(".png","")
+    res_bd = db.registerUser(name_user, path + img)
+
+    getEnter(screen1)
+    if(res_bd["affected"]):
+        printAndShow(screen1, "¡Éxito! Se ha registrado correctamente", 1)
+    else:
+        printAndShow(screen1, "¡Error! No se ha registrado correctamente", 0)
+    os.remove(img)
+
+def register_capture():
+    cap = cv2.VideoCapture(0)
+    user_reg_img = user1.get()
+    img = f"{user_reg_img}.jpg"
+
+    while True:
+        ret, frame = cap.read()
+        cv2.imshow("Registro Facial", frame)
+        if cv2.waitKey(1) == 27:
+            break
+    
+    cv2.imwrite(img, frame)
+    cap.release()
+    cv2.destroyAllWindows()
+
+    user_entry1.delete(0, END)
+    
+    pixels = plt.imread(img)
+    faces = MTCNN().detect_faces(pixels)
+    face(img, faces)
+    register_face_db(img)
+
+def register():
+    global user1
+    global user_entry1
+    global screen1
+
+    screen1 = Toplevel(root)
+    user1 = StringVar()
+
+    configure_screen(screen1, txt_register)
+    user_entry1 = credentials(screen1, user1, 0)
+
+# LOGIN #
+def compatibility(img1, img2):
+    orb = cv2.ORB_create()
+
+    kpa, dac1 = orb.detectAndCompute(img1, None)
+    kpa, dac2 = orb.detectAndCompute(img2, None)
+
+    comp = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+    matches = comp.match(dac1, dac2)
+
+    similar = [x for x in matches if x.distance < 70]
+    if len(matches) == 0:
+        return 0
+    return len(similar)/len(matches)
+
+def login_capture():
+    cap = cv2.VideoCapture(0)
+    user_login = user2.get()
+    img = f"{user_login}_login.jpg"
+    img_user = f"{user_login}.jpg"
+
+    while True:
+        ret, frame = cap.read()
+        cv2.imshow("Login Facial", frame)
+        if cv2.waitKey(1) == 27:
+            break
+    
+    cv2.imwrite(img, frame)
+    cap.release()
+    cv2.destroyAllWindows()
+
+    user_entry2.delete(0, END)
+    
+    pixels = plt.imread(img)
+    faces = MTCNN().detect_faces(pixels)
+
+    face(img, faces)
+    getEnter(screen2)
+
+    res_db = db.getUser(user_login, path + img_user)
+    if(res_db["affected"]):
+        my_files = os.listdir()
+        if img_user in my_files:
+            face_reg = cv2.imread(img_user, 0)
+            face_log = cv2.imread(img, 0)
+
+            comp = compatibility(face_reg, face_log)
+            
+            if comp >= 0.94:
+                print("{}Compatibilidad del {:.1%}{}".format(color_success, float(comp), color_normal))
+                printAndShow(screen2, f"Bienvenido, {user_login}", 1)
+            else:
+                print("{}Compatibilidad del {:.1%}{}".format(color_error, float(comp), color_normal))
+                printAndShow(screen2, "¡Error! Incopatibilidad de datos", 0)
+            os.remove(img_user)
+    
+        else:
+            printAndShow(screen2, "¡Error! Usuario no encontrado", 0)
+    else:
+        printAndShow(screen2, "¡Error! Usuario no encontrado", 0)
+    os.remove(img)
+
+def login():
+    global screen2
+    global user2
+    global user_entry2
+
+    screen2 = Toplevel(root)
+    user2 = StringVar()
+
+    configure_screen(screen2, txt_login)
+    user_entry2 = credentials(screen2, user2, 1)
+
+root = Tk()
+root.geometry(size_screen)
+root.title("AVM")
+root.configure(bg=color_background)
+Label(text="¡Bienvenido(a)!", fg=color_white, bg=color_black, font=(font_label, 18), width="500", height="2").pack()
+
+getEnter(root)
+Button(text=txt_login, fg=color_white, bg=color_black_btn, activebackground=color_background, borderwidth=0, font=(font_label, 14), height="2", width="40", command=login).pack()
+
+getEnter(root)
+Button(text=txt_register, fg=color_white, bg=color_black_btn, activebackground=color_background, borderwidth=0, font=(font_label, 14), height="2", width="40", command=register).pack()
+
+root.mainloop()
